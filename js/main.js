3eabbbfe9670b52f4bdadf9f0538d342cb89f76c
violetagonza <violetagonzalezcalleja@gmail.com>
2020-03-10 09:49:50 +0100

    Handle number comparation with click on the button and paint clue message

--- js/main.js
+++ js/main.js
@@ -19,7 +19,24 @@ function handleInput() {
 const getClueText = document.querySelector('.clue');
 console.log(getClueText);
 
-function compareNumbers() {}
+// Compara números
+function compareNumbers() {
+  debugger;
+  if (parseInt(getInput.value) > 100 || parseInt(getInput.value) < 1) {
+    getClueText.innerHTML = 'El número debe estar entre 1 y 100';
+  } else if (parseInt(getInput.value) === randomNumber) {
+    getClueText.innerHTML = 'Has ganado campeona!!!';
+  } else if (getInput.value > randomNumber) {
+    getClueText.innerHTML = 'Demasiado alto';
+  } else if (getInput.value < randomNumber) {
+    getClueText.innerHTML = 'Demasiado bajo';
+  }
+}
+function handleButton() {
+  event.preventDefault();
+  handleInput();
+  compareNumbers();
+}
 
 // Escucha button
-button.addEventListener('click', handleInput);
+button.addEventListener('click', handleButton);
